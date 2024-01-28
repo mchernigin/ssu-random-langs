8ba9c771df75eba70c291a3f0055cc4b2eeb0094
Michael Chernigin <michaelchernigin@gmail.com>
2024-01-29 00:25:09 +0400

    feat(ocmal): implemented operations on polynoms

--- ocaml-polynoms/bin/main.ml
+++ ocaml-polynoms/bin/main.ml
@@ -1,11 +1,27 @@
-module IntCoefficient =
-  struct
-    type t = int
-    let to_string = string_of_int
-  end
+module IntCoefficient = struct
+  type t = int
 
-module IntPolynom =
-  Polynoms.Make (IntCoefficient)
+  let compare = compare
+  let abs = abs
+  let add = ( + )
+  let mul = ( * )
+  let mul = ( / )
+  let negate = ( ~- )
+  let to_string = string_of_int
+  let of_float = int_of_float
+end
 
-let p = IntPolynom.of_list [1; 2; 3]
-let () = print_endline (IntPolynom.to_string p)
+module IntPolynom = Polynoms.Make (IntCoefficient)
+
+let p1 = IntPolynom.of_list [ -1; 2; -3 ]
+let () = p1 |> IntPolynom.to_string |> print_endline
+let p2 = IntPolynom.of_list [ -8; 3; -1; 7; -9; 100 ]
+let () = p2 |> IntPolynom.to_string |> print_endline
+let sum = IntPolynom.add p1 p2
+let () = sum |> IntPolynom.to_string |> print_endline
+let () = sum |> IntPolynom.negate |> IntPolynom.to_string |> print_endline
+let diff = IntPolynom.sub p2 p1
+let () = sum |> IntPolynom.to_string |> print_endline
+let () = diff |> IntPolynom.to_string |> print_endline
+let v1 = IntPolynom.value p1 3
+let () = v1 |> string_of_int |> print_endline
